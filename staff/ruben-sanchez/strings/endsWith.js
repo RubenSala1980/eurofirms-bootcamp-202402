6d9c980976afb0986d47453585207cc150d685bb
Ruben Sanchez <rubensala1980@gmail.com>
2024-03-14 11:06:14 +0100

    added code lines #150

--- staff/ruben-sanchez/strings/endsWith.js
+++ staff/ruben-sanchez/strings/endsWith.js
@@ -1,12 +1,24 @@
 var string = 'The last is also the first, the only one'
 
-function endsWith(string, word) {
+function endsWith(string, word,index=string.length) {
     var tree = ''
-    for (var i = 0; i < string.length; i++) { 
-        for (var j=0; i<word.length; j++) {
-            word === treeg[j]
-        }
+     
+    
 
+  
+    for (var i =index-word.length ; i < string.length; i++) { 
+      console.log( tree += string[i])
     }
-    return false
+    if(tree === word){
+        return console.log(true)
+    }
+
+    return console.log(false)
+
 }
+//caso 1 string , one return true
+endsWith(string, 'one')
+// caso 2 strin, 'barba' return false
+endsWith(string, 'barba')
+// caso 3 string, 'one' , 12, return false
+endsWith(string, 'one', 12)
---
+++ staff/ruben-sanchez/strings/replace.js
@@ -0,0 +1,9 @@
+var string = 'The new order is like the old one'
+
+function replace(string, word, replacement) {
+    var comparator = ''
+    for (var i = 0; i < string.length; i++) {
+        comparatorb
+
+    }
+}
