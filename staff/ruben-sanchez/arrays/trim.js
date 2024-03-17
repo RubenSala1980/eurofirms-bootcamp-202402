ae4db73d329da334afc23ba74d04b2fae9024f91
Ruben Sanchez <rubensala1980@gmail.com>
2024-03-17 16:07:36 +0100

    added the function trim transcripted #171

--- staff/ruben-sanchez/arrays/trim.js
+++ staff/ruben-sanchez/arrays/trim.js
@@ -1,25 +1,41 @@
-function trim(string){
-    // creamos una variaable donde iremos guardando nuestro nuevo string, que inicializamos con un string vacio
-    // recorrer el string con un bucle
-    // si encontramos espacios antes de llegar al primer caracater, no lo agregamos a nuestron nuevo string
-    // la accion anterior la repetimos hasta encontar un caracter que no sea un espacio
-    var result = ' '
+function trim(string) {
+   
     var index1 = 0
-    for(var i=0;i<string.lenght;i++){
-     
-        if(string[i] === ' '){
-            index1 += i
+    var index2 = 0
+    var arr = ''
+    for (var i = 0; i < string.length; i++) {
+       
+        if (string[i] !== ' ') {
+            index1 = i
+           break
             
-           console.log(index1)
-          break
+          
         }
         
     }
+    for (var i = string.length-1;i>0;i--){
+        if (string[i] !== ' ') {
+            index2 = i
+           break
+            
+          
+        }
 
-return console.log(index1)
-   
-   
+    }
+    for (var i= index1; i <= index2; i++){
+       arr += string[i]
+    }
+console.log(arr)
+
+
+
+
 }
+
+
+
+
+
 
 console.log('FUNCTION TRIM')
 
@@ -27,7 +43,7 @@ console.log('CASE 1: delete empty sapces form beginin and end of "  Hello World 
 
 var helloWorld = '  Hellow World  '
 
-var result = helloWorld.trim()
 
-console.log(result) // 'Hello World'
+
+console.log() // 'Hello World'
 trim(helloWorld)
