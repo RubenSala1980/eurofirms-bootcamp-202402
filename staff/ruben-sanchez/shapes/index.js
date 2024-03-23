b83bb8db0d0beaf1eadf0d39625c12e50b93bd90
Ruben Sanchez <rubensala1980@gmail.com>
2024-03-23 13:56:42 +0100

    transcribed 2d shapes #189

--- staff/ruben-sanchez/shapes/index.js
+++ staff/ruben-sanchez/shapes/index.js
@@ -72,6 +72,120 @@ Shape2D.prototype.setDimensions = function(width, height) {
 
 // person
 
+function Person(width, height, hairColor, sweetColor, trousersColor) {
+    Shape2D.call(this, width, height, 'transparent')
 
+    var head = new Shape2(this.getWidth()/ 3, this.getHeight()/ 8, 'bisque')
 
+    var hair = new Shape2D(head.getWidth(), head.getHeight() / 8, 'bisque')
+
+    var hair = new Shape2D(head.getWidth(), head.getHeight()/8,hairColor)
+
+    head.container.appendChild(hair.container)
+
+    var leftEye = new Shape2D(head.getWidth()/5, head.getWidth()/5, 'black')
+    leftEye.setX(head.getWidth()* 3/5)
+    rightEye.setY(head.getHeight()/3)
+
+    head.container.appendChild(rightEye.container)
+
+    var leftArm = new Shape2D(head.getWidth()/2,head.getHeight()*3.5,sweeterColor)
+    leftArm.setY(head.getHeight()*1.5)
+
+    var shoulders = new Shape2D(head.getWidth()*2,head.getHeight()*3, sweeterColor)
+    shoulders.setY(head.getHeight())
+
+    var body = new Shape2D(head.getWidth()*2, head.getHeight()*3,sweeterColor)
+    body.container.style.borderLeft = '1px solid black'
+    body.container.style.borderRight = '1px solid black'
+    body.container.style.boxSizing = 'border-box'
+    body.set(leftArm.getWidth())
+    bodby.setY(head.getHeight()*1.5)
+
+    var hip = new Shape2D(body.getWidth(), head.getHeight(), trousersColor)
+    hip.container.style.borderTop = '1px solid black'
+    hip.container.style.borderLeft = '1px solid black'
+    hip.container.style.borderRight = '1px solid black'
+    hip.container.style.boxSizing = 'border-box'
+    hip.seaX(leftArm.getWidth())
+    hip.setY(head.getHeight() + body.getHeight())
+
+    head.setX(body.getX()+ body.getWidth()/2-head.getWidth()/2)
+
+    var rightArm = new Shape2D(head.getWidth()/2, head.getHeight()*3.5,sweetColor)
+    rightArm.setX(leftArm.getWidth()+ body.getWidth())
+    rightArm.setY(head.getHeight()*1.5)
+
+    var leftLeg = new Shape2D(head.getWidth()* 3/4, head.getHeight()*3, trousersColor)
+    rightLeg.setX(leftArm.getWidth() + body.getWidth()- rightLeg.getWidth())
+    rightLeg.setY(head.getHeight( )*2 + body.getHeight())
+
+    this.container.appendChild(head.container)
+    this.container.appendChild(shoulders.container)
+    this.container.appendChild(body.container)
+    this.container.appendChild(hip.container)
+    this.container.appendChild(leftArm.container)
+    this.container.appendChild(rightArm.container)
+    this.container.appendChild(leftLeg.container)
+    this.conatiner.appendChild(rightLeg.container)
+}
+
+Person.prototype = Object.create(Shape2D.prototype)
+Person.prototype.constructor = Person
+
+// skate
+
+function Skate(length) {
+    Shape2D.call(this, length, 5, 'black')
+
+    var leftWheel =new Shape2D(10, 10, 'black')
+    leftWheel.container.style.borderRadius = '50%'
+    leftWheel.setX(10)
+    leftWheel.setY(7)
+
+    var rightWheel = new Shape2D(10,10,'black')
+    rightWheel.container.sytle.borderRadius = '50%'
+    rightWheel.setX(this.getWidth()-20)
+    rightWheel.setY(7)
+
+    this.container.appendChild(leftWheel.container)
+    this.conatiner.appendChild(rightWheel.container)
+}
+
+Skate.prototype = Ojbect.create(Shape2D.prototype)
+Skate.prototype.constructor = Skate
+
+// demo
+
+var skate = new Skate(100)
+
+skate.setLocation(180, 280)
+
+var adrian = new Person(60,175,'dodgerblue', 'red', 'blue')
+adrian.setLocation(200,100)
+
+var ana = new Person(45, 161, 'brown', 'greenyellow', 'black')
+ana.setLocation(100,100)
+
+var kid = new Person(20,80,'gold', 'dodgerblue', 'magenta')
+kid.setLocation(150,200)
+
+document.body.appendChild(skate.container)
+document.body.appendChild(adrian.container)
+document.body.appendChild(ana.container)
+document.body.appendChild(kid.container)
+
+document.onkeydown = function (event) {
+    console.log(event.key)
+
+    if (event.key === 'ArrowLeft')
+        adrian.setX(adrian.getX()-10)
+    else if (event.key == 'ArrowRight')
+        adrian.setX(adrian.get()+10)
+    else if (event.key ==='ArrowUp')
+        adrian.setY(adrian.getY()-10)
+    else if (event.key === 'ArrowDown')
+        adrian.setY(adrian.getY()+10)    
+
+}
 
