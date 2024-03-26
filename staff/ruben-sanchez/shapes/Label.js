ab8cd4eadd2f505ecf0edba39c511c61030c4192
Ruben Sanchez <rubensala1980@gmail.com>
2024-03-26 07:09:28 +0100

    transcribed hangman #86

---
+++ staff/ruben-sanchez/shapes/Label.js
@@ -0,0 +1,11 @@
+function Label() {
+    Component.call(this, 'label')
+
+}
+
+Label.prototype = Object.create(Component.prototype)
+Label.prototype.constructor = Label
+
+Label.prototype.setFor = function (htmlFor) {
+    this.conatainer.htmlFor = htmlFor
+}
